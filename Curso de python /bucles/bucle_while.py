134da1b878a6428f930a8e6eda654500176193bc
movacx <fabianbeita36@gmail.com>
2026-01-11 16:17:09 -0600

    .

---
+++ Curso de python /bucles/bucle_while.py
@@ -0,0 +1,10 @@
+#Creando un contador que se va a ir sumando
+contador = 0
+
+#Mientras que la condicion se cumpla, el bucle se va a seguir ejecutando
+#vuelta, tras vuelta se verifica la condicion
+while contador < 11:
+    print(contador)
+    contador +=1
+    
+print("El contador llego a su fin")
