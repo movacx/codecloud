da5eb17deaf14acd3786c584411f2d5284f1aee3
movacx <fabianbeita36@gmail.com>
2026-01-12 19:51:47 -0600

    update menu

--- Tareas/10_ejercicios 12-1/menu_con_funciones.py
+++ Tareas/10_ejercicios 12-1/menu_con_funciones.py
@@ -3,8 +3,23 @@
 
 contador = 0
 while contador != 3:
-    print("Seleccione una opcion: \n",
-        "1. Saludar",
-        "2. Sumar dos numeros",
-        "3. Salir")
+    print("\nMenu Principal",
+        "\n1. Saludar",
+        "\n2. Sumar dos numeros",
+        "\n3. Salir")
+    opcion_menu = int(input("Seleccion una opcion [1-3]: "))
+    
+    if opcion_menu == 1:
+        print("Hola!")
+    elif opcion_menu == 2:
+        num_uno = int(input("Ingrese el primer valor: "))
+        num_dos = int(input("Ingrese el primer valor: "))
+        resultado = num_uno + num_dos
+        print(f"El resultado de la suma fue de: {resultado}")
+    elif opcion_menu == 3:
+        break
+    else:
+        print("Seleccione una opcion valida")
+    
+    
     
