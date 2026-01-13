8e4e3f15a9c95de7f80a2d7e215eb47081230eb0
movacx <fabianbeita36@gmail.com>
2026-01-12 19:35:22 -0600

    d

---
+++ Tareas/10_ejercicios 12-1/menu_con_funciones.py
@@ -0,0 +1,10 @@
+#Implemente un menú con:
+    #1. Saludar 2. Sumar dos números 3. Salir
+
+contador = 0
+while contador != 3:
+    print("Seleccione una opcion: \n",
+        "1. Saludar",
+        "2. Sumar dos numeros",
+        "3. Salir")
+    
