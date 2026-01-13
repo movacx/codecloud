d7c1f67b3d7bb2e7791791a66700dd5f2c86304e
movacx <fabianbeita36@gmail.com>
2026-01-13 12:12:05 -0600

    a

--- Desarrollo de software II/semana II/dia 3/reto arregloClase/arregloClase.py
+++ Desarrollo de software II/semana II/dia 3/reto arregloClase/arregloClase.py
@@ -1,104 +1,68 @@
-#Mi variables
-           #0      ,1        ,2          ,3
-personas = ["Mario", "Isaac", "Alberto","Sasha"]
+class Persona:
+    def __init__(self, nombre, edad):
+        self._nombre = nombre
+        self._edad = edad
+    
+    # Getters y Setters
+    def get_nombre(self):
+        return self._nombre
+    
+    def set_nombre(self, nombre):
+        self._nombre = nombre
+        
+    def get_edad(self):
+        return self._edad
+        
+    def set_edad(self, edad):
+        self._edad = edad
+
+    def __repr__(self): 
+        return f"{self._nombre}"
+
+personas = [] 
+
+personas.append(Persona("Mario", 20))
+personas.append(Persona("Isaac", 22))
+personas.append(Persona("Alberto", 25))
+personas.append(Persona("Sasha", 30))
+
 textoRespuesta = ""
 
-#Funciones
 def mostrar():
-   print("Los usuarios del sistema: ", personas)
+    print("Los usuarios del sistema: ", personas)
 
 def buscar(usuarioNombre):
-    if usuarioNombre in personas:
-        indice = personas.index(usuarioNombre)
-    else:
-        indice =-1
-    return indice
+    indice_encontrado = -1
+    contador = 0
+    
+    for obj_persona in personas:
+        if obj_persona.get_nombre() == usuarioNombre:
+            indice_encontrado = contador
+            break 
+        contador = contador + 1
+    return indice_encontrado
 
-def buscarAlumno():
-    print("Buscar")
-    nombreBuscar=input("Ingrese el nombre a buscar en la lista: ")
-    if nombreBuscar in personas:
-      indice=personas.index(nombreBuscar)
-      print(nombreBuscar,"Si se encuentra en la lista")
-    else:
-      print("No se encuentra el nombre ingresado en la lista")  
-       
+def modificar(nuevoNombre, indice):
+    personas[indice].set_nombre(nuevoNombre)
+
 def borrarAlumno(nombre):
-    #un if para verificar y eliminar
-    #para evitar duplicar codigo
-    # if usuarioNombre in personas:
-    #     indice =personas.index(usuarioNombre)
-    # else:
-    #     indice =-1
-    personas.remove(nombre)
-    print("El nombre: ",nombre," fue Eliminado")
-
-def buscarDos(personas,nombre):
-    if nombre in personas:
-       print("el usuario si esxiste")
-    else:
-       print("el usuario no existe")
-
-def modificarAlumno(nombre, nombreModificar):
-    indice = personas.index(nombre)
-    personas[indice] = nombreModificar
-    print("Modificado")
-
-def modificar(nombreModificar, indice):
-   personas[indice] = nombreModificar
-
-def borrar(nombre):
-    print("Borrar")
-    indice = -1
-    return indice    
+    posicion = buscar(nombre)
+    if posicion != -1:
+        # pop elimina el objeto en esa posicion
+        personas.pop(posicion) 
+        print("El nombre:", nombre, "fue Eliminado")
 
 mostrar()
-
-usuarioNuevo = input("Ingrese un nuevo usuario: ")
-personas.append(usuarioNuevo)#Insertar elementos en un arreglo
-
-mostrar()
-
-usuarioNombre = input("Ingrese el usuario a buscar: ")
-posicion = buscar(usuarioNombre)
-
-if posicion == -1:
-    textoRespuesta = "No encontro datos para", usuarioNombre
-else:
-    textoRespuesta = "Se encontro datos para", usuarioNombre, "en el identificador: ", posicion
-
-print (textoRespuesta)
-
-usuarioNombreBorrar = input("Ingrese el usuario a buscar y eliminar: ")
-
-posicion = buscar(usuarioNombreBorrar)
-
-if posicion == -1:
-    textoRespuesta = "No encontro datos para", usuarioNombreBorrar
-else:
-    borrarAlumno(usuarioNombreBorrar)
-    textoRespuesta = "Se encontro datos para", usuarioNombreBorrar    
-
-print(textoRespuesta)
-
-
 usuarioNombreModificar = input("Ingrese el usuario a buscar y modificar: ")
+
 posicion = buscar(usuarioNombreModificar)
 
 if posicion == -1:
     textoRespuesta = "No encontro datos para", usuarioNombreModificar
 else:
     nuevoNombre = input("Ingrese el nuevo nombre: ")
+   
     modificar(nuevoNombre, posicion)
     textoRespuesta = "Se encontro datos para", usuarioNombreModificar    
-
 print(textoRespuesta)
 mostrar()
-
-
-#La seccion de buscar
-#La seccion de modificar
-#La seccion de borrar
-#= es para asignar un valor
-#== es para comparar un valor
-#=== es para comparar un valor + el tipo de dato
---
+++ Desarrollo de software II/semana II/dia 3/reto arregloClase/clasePersona.py
@@ -0,0 +1,41 @@
+class persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+   
+        
+lista_personas = []
+
+#Funciones
+def mostrar():
+   print("Los usuarios del sistema: ", lista_personas)
+
+def buscar(usuarioNombre):
+    if usuarioNombre in lista_personas:
+        indice = lista_personas.index(usuarioNombre)
+    else:
+        indice =-1
+    return indice
+
+def borrar(nombre):
+    lista_personas.remove(nombre)
+    print("El nombre: ",nombre," fue Eliminado")
+
+def modificar(nombreModificar, indice):
+   lista_personas[indice] = nombreModificar
+
+
+def insertar(nombre, edad):
+    nuevosDatos = persona(nombre, edad)
+    lista_personas.append(nuevosDatos)
+    print("Datos almacenados exitosamente")
+    
+#######################################################
+
+nombre = input("Ingrese el nombre de la persona: ")
+edad = input("Ingrese la edad de las personas: ")
+
+insertar(nombre, edad)
+mostrar()
+
+print(lista_personas)
