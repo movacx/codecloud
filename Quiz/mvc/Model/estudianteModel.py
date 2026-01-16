892fe92b2b8725d27c81cb9f864e2d749cf680c8
movacx <fabianbeita36@gmail.com>
2026-01-15 19:12:57 -0600

    ss

---
+++ Quiz/mvc/Model/estudianteModel.py
@@ -0,0 +1,7 @@
+#nombre, edad, grado, correo
+class Estudiante:
+    def __init__(self, nombre, edad, grado, correo):
+        self.nombre = nombre
+        self.edad = edad
+        self
+        pass
