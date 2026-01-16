4bd2aa082aff13a14abc14b1c7869df13955180f
movacx <fabianbeita36@gmail.com>
2026-01-16 13:50:58 -0600

    f

--- Quiz/mvc/principal.py
+++ Quiz/mvc/principal.py
@@ -1,11 +1,30 @@
 from Controller.estudianteController import EstudianteController
 
 
-def main():
+def maind():
       while True:
-            print("Hola mundo")
+            opcion = int(input("""
+            1. Gestión de Estudiantes
+            2. Gestión de Profesores
+            3. Gestión de Cursos
+            4. Salir 
+            input: """))
+            
+            if opcion == 1:
+                  continue
+            elif opcion == 2:
+                  continue
+            elif opcion == 3:
+                  continue
+            elif opcion == 4:
+                  break
+            else:
+                  print("Opcion Invalida [1-4]")
+                  
+      return " "
+            
+print(maind())
       
       
       
       
-print(main())
