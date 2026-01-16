76eeac5763b748a5f3c15cd54115c57baa038779
movacx <fabianbeita36@gmail.com>
2026-01-15 19:40:25 -0600

    d

--- Quiz/mvc/Model/estudianteModel.py
+++ Quiz/mvc/Model/estudianteModel.py
@@ -1,7 +1,37 @@
 #nombre, edad, grado, correo
 class Estudiante:
+    idEstudiante = 0
     def __init__(self, nombre, edad, grado, correo):
         self.nombre = nombre
         self.edad = edad
-        self
-        pass
+        self.grado = grado
+        self.correo = correo
+        Estudiante.idEstudiante =+1
+        self.id = Estudiante.idEstudiante
+        
+    #Getters
+    def getNombre(self):
+        return self.nombre
+    def getEdad(self):
+        return self.edad
+    def getGrado(self):
+        return self.grado
+    def getCorreo(self):
+        return self.correo
+    
+    #Setters
+    def setNombre(self, nombre):
+        self.nombre = nombre
+    def setEdad(self, edad):
+        self.edad = edad
+    def setGrado(self, grado):
+        self.grado = grado
+    def setCorreo(self, correo):
+        self.correo = correo
+        
+    def __str__(self):
+        return f"""
+    """
+        
+    
+    
---
+++ Quiz/mvc/Model/profesorModel.py
@@ -0,0 +1,41 @@
+#nombre, especialidad, telefono, correo
+class Profesor:
+    idProfesor = 0
+    def __init__(self, nombre, especialidad, telefono, correo):
+        self.nombre = nombre
+        self.especialidad = especialidad
+        self.telefono = telefono
+        self.correo = correo
+        Profesor.idProfesor =+1
+        self.id = Profesor.idProfesor
+        
+    #Getters
+    def getNombre(self):
+        return self.nombre
+    def getEspecialidad(self):
+        return self.especialidad
+    def getTelefono(self):
+        return self.telefono
+    def getCorreo(self):
+        return self.correo
+    
+    #Setters
+    def setNombre(self, nombre):
+        self.nombre = nombre
+    def setEspecialidad(self, especialidad):
+        self.especialidad = especialidad
+    def setTelefono(self, telefono):
+        self.telefono = telefono
+    def setCorreo(self, correo):
+        self.correo = correo
+        
+    def __str__(self):
+        return f"""
+    Nombre: {self.nombre}
+    Especialidad: {self.especialidad}
+    Telefono: {self.telefono}
+    Correo: {self.correo}"""
+    
+    def mostrarEstudiantes(self):
+        return self.nombre, self.especialidad, self.telefono, self.correo
+        
