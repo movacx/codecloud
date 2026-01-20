2a8401808a43545fbd30370ee71c3f1ba073a04a
movacx <fabianbeita36@gmail.com>
2026-01-20 12:24:04 -0600

    d

---
+++ Actividades/evaluaciones/Laboratorio/mvc/Model/cursosModel.py
@@ -0,0 +1,46 @@
+#Participantes: 
+#Herlin Fabian Chavarria Beita C5E187
+#Joseph Campos C4D660
+#David Mora Gomez C5H441
+
+class Curso: 
+    
+    idCurso = 0
+    def __init__(self, nombreCurso,codigo, profesorAsignado):
+        self.nombreCurso = nombreCurso
+        self.codigo = codigo
+        self.profesorAsignado = profesorAsignado
+        Curso.idCurso += 1
+        self.id = Curso.idCurso
+
+    #-----------------------GETS-----------------------#  
+    def getNombreCurso(self):
+        return self.nombreCurso
+    
+    def getCodigo(self):
+        return self.codigo
+    
+    def getProfesorAsignado(self):
+        return self.profesorAsignado 
+    
+    #-----------------------GETS-----------------------#  
+    def setNombreCurso(self,nombreCurso):
+        self.nombreCurso = nombreCurso
+    
+    def setCodigo(self,codigo):
+        self.codigo = codigo
+    
+    def setProfesorAsignado(self,profesorAsignado):
+        self.profesorAsignado = profesorAsignado 
+    
+    #To String
+
+    def __str__(self):
+        return f"""
+    Nombre: {self.nombreCurso}
+    Edad: {self.codigo}
+    Grado:  {self.profesorAsignado}
+    """
+
+    def mostrarCursos(self):
+        return self.nombreCurso, self.codigo, self.profesorAsignado
