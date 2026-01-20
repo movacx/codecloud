13142f382fad1ad09a44830ae5dacdff195d0b92
movacx <fabianbeita36@gmail.com>
2026-01-20 13:31:03 -0600

    ddfg

--- Actividades/evaluaciones/Polimorfismo&herencia/Laboratorio 20_01/mvc/controller/estudianteController.py
+++
@@ -1,9 +0,0 @@
-from model.estudianteModel import Estudiante
-
-class EstudianteController:
-    def __init__(self):
-        self.lista = []
-    
-    #------------------Mostrar Datos----------------------------
-    def mostrarDatos(self):
-        
