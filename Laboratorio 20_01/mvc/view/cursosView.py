8eb37470d12ef909aa244a18ab93ba7898f911e6
movacx <fabianbeita36@gmail.com>
2026-01-20 15:52:04 -0600

    d

--- Laboratorio 20_01/mvc/view/cursosView.py
+++ Laboratorio 20_01/mvc/view/cursosView.py
@@ -8,9 +8,7 @@ from view.comunesView import mostrarMensaje, mostrarTodos, mostrarUno
 
 def gestionCursos():
     print("""
-    ==============================
          GESTIÓN DE CURSOS
-    ==============================
     1. Crear Curso
     2. Listar Cursos
     3. Modificar Curso
