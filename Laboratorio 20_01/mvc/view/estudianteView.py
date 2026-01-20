9083593a4605cdc8fa5d371df5a07e5a19f5f620
movacx <fabianbeita36@gmail.com>
2026-01-20 15:36:14 -0600

    s

--- Laboratorio 20_01/mvc/view/estudianteView.py
+++ Laboratorio 20_01/mvc/view/estudianteView.py
@@ -1,29 +1,30 @@
-#Participantes: 
-#Herlin Fabian Chavarria Beita C5E187
-#Joseph Campos C4D660
-#David Mora Gomez C5H441
+# Participantes: 
+# Herlin Fabian Chavarria Beita C5E187
+# Joseph Campos C4D660
+# David Mora Gomez C5H441
 
 # Importamos lo común
 from view.comunesView import mostrarMensaje, mostrarTodos, mostrarUno
 
-def gestionProfesor():
+def gestionEstudiante():
     print("""
     ==============================
-       GESTIÓN DE PROFESORES
+       GESTIÓN DE ESTUDIANTES
     ==============================
-    1. Registrar profesor
-    2. Listar profesores
-    3. Actualizar profesor
-    4. Eliminar profesor
-    5. Volver al Menú Principal
-    """)
-
+    1. Registrar estudiante
+    2. Listar estudiantes
+    3. Buscar estudiante
+    4. Actualizar estudiante
+    5. Eliminar estudiante
+    6. Volver al Menú Principal
+    input: """)
+    
 def MenuModificacion():
     print("""
-    --- MODIFICAR PROFESOR ---
+    --- MODIFICAR ESTUDIANTE ---
     1. Modificar Nombre
-    2. Modificar Especialidad
-    3. Modificar Teléfono
+    2. Modificar Edad
+    3. Modificar Grado
     4. Modificar Correo
     0. Cancelar
     """)
