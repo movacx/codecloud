06a42f6232b4d66eefbaf724d6f05b7e8431328b
Davox1095 <davidmorag1095@gmail.com>
2026-01-23 13:18:53 -0600

    asd

--- Laboratorio #2/mvc/Model/productosModel.py
+++ Laboratorio #2/mvc/Model/productosModel.py
@@ -1,1 +1,8 @@
-asd
+class Productos:
+	
+	def __init__(self, codigoUnico,nombre, Categoria,precio,stock):
+		self.codigoUnico = codigoUnico
+		self.nombre = nombre
+		self.categoria = categoria
+		self.precio = precio
+		self.stock = stock
