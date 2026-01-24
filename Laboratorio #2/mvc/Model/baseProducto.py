bd71a452fbe0d96e692410dfa7a8a01e1356f384
Davox1095 <davidmorag1095@gmail.com>
2026-01-24 10:12:10 -0600

    asd

--- Laboratorio #2/mvc/Model/baseProducto.py
+++ Laboratorio #2/mvc/Model/baseProducto.py
@@ -1,103 +1,89 @@
 import sys
-
 import os
+
+sys.stdout.reconfigure(encoding = "utf-8")
 
 import csv
 
-sys.stdout.reconfigure(encoding="utf-8")
-
 BASE_DIR = os.path.dirname(__file__)
+ARCHIVO = os.path.join(BASE_DIR, "CSV", "productos.csv")
 
-FOLDER = os.path.join(BASE_DIR, 'csv')
+#Metodo guardar Producto
+def guardarProducto(id, nombre, categoria, precio, stock):
+	
+	with open(ARCHIVO, "a", newline="", encoding= "utf-8") as archivoParaGuardar:
+		writer = csv.writer(archivoParaGuardar)
+		
+	print ("Se guardaron los productos")
+	
+	
+#Listar productos
+def listarProductos():
+	with open(ARCHIVO, "r", enconding= "utf-8") as archivoparaleer:
+		reader = csv.reader(archivoparaleer)
+		for item in reader:
+			print(item)
+	
+#Buscar por ID
+def buscarProductoId(id):
+	with open(ARCHIVO, "r", encoding = "utf-8") as archivoparaleer:
+		reader = csv.reader(archivoParaLeer)
+	
+		for item in reader:
+			if item[0] == str(id):
+				return item
+			
+#Modificar Producto
+def modificarProducto(id, nombre, categoria, precio, stock):
+	
+	arregloVacio = [ ]
+	
+	with open(ARCHIVO, "r", newline="", encoding="utf-8") as archivoParaLeer:
+		reader = csv.reader(archivoParaLeer)
+		for item in reader:
+			if item[0] == str(id):
+				item[1] = nombre
+				item[2] = categoria
+				item[3] = precio
+				item[4] = stock
+			
+		arrregloVacio.append(item)
+		
+		with open(ARCHIVO, "w", nenewline="", encoding= "utf-8") as archivoParaEscribir:
+			writer = writer.writerrow(arregloVacio)				
 
-ARCHIVO = os.path.join(FOLDER, 'productos.csv')
 
-def guardarProducto(codigoUnico, nombre, categoria, precio, stock):
+#Eliminar producto
+def eliminarProducto(id):
+	arregloVacio = [ ]
     
-    with open(ARCHIVO, "a", newline="", encoding="utf-8") as archivoparaguardar:
-        
-        writer = csv.writer(archivoparaguardar)
-        writer.writerow([codigoUnico, nombre, categoria, precio, stock])
-        
-        print(f"Producto {nombre} guardado exitosamente.")
+	with open(ARCHIVO, "r", encoding="utf-8") as archivoParaLeer:
+		reader = csv.reader(archivoParaLeer)
+	
+		for item in reader:
+			if item[0] != str(id):
+				arregloVacio.append(item)
 
-def mostrarProductos():
-    
-    with open(ARCHIVO, "r", newline="", encoding="utf-8") as archivoparaleer:
-        
-        reader = csv.reader(archivoparaleer)
-        
-        for item in reader:
-            print(item)
-
-def mostrarProductoId(codigoUnico):
-    
-    with open(ARCHIVO, "r", newline="", encoding="utf-8") as archivoparaleer:
-        
-        reader = csv.reader(archivoparaleer)
-       
-        for item in reader:
-            if item[0] == str(codigoUnico):
-                return item
-        return "No encontrado"
-
-def modificarProducto(codigoUnico, nombre, categoria, precio, stock):
-    arregloVacio = []
-    encontrado = False
-    
-    with open(ARCHIVO, "r", newline="", encoding="utf-8") as archivoparaleer:
-        
-        reader = csv.reader(archivoparaleer)
-        
-        for item in reader:
-            if item[0] == str(codigoUnico):
-                
-                item = [codigoUnico, nombre, categoria, precio, stock]
-                encontrado = True
-            arregloVacio.append(item)
-            
-    if encontrado:
-        with open(ARCHIVO, "w", newline="", encoding="utf-8") as archivoparaescribir:
-            
-            writer = csv.writer(archivoparaescribir)
-            writer.writerows(arregloVacio)
-        
-        print(f"Producto {codigoUnico} actualizado.")
-    else:
-        print("Producto no encontrado para modificar.")
-
-def eliminarProductos(codigoUnico):
-    arregloVacio = []
-
-    with open(ARCHIVO, "r", newline="", encoding="utf-8") as archivoparaleer:
-        
-        reader = csv.reader(archivoparaleer)
-        
-        for item in reader:
-            if item[0] != str(codigoUnico):                                
-                arregloVacio.append(item)
-            
-    with open(ARCHIVO, "w", newline="", encoding="utf-8") as archivoparaescribir:
-        
-        writer = csv.writer(archivoparaescribir)
-        writer.writerows(arregloVacio)
-    
-    print(f"Eliminado el producto: {codigoUnico}")
+	with open(ARCHIVO, "w", newline="", encoding="utf-8") as archivo:
+			csv.writer(archivo).writerows(productos)
 
 
-guardarProducto(2, "sonido", "Peri", 250, 10)
-guardarProducto(3, "compu", "Peri", 100, 20)
 
 
-print("Buscando  3:", mostrarProductoId(3))
 
 
-modificarProducto(3, "compu", "Peri", 400, 5)
 
 
-eliminarProductos(2)
 
 
-print("Lista final:")
-mostrarProductos()
+
+
+
+
+
+
+
+
+
+
 
--- Laboratorio #2/mvc/Model/productosModel.py
+++ Laboratorio #2/mvc/Model/productosModel.py
@@ -1,67 +1,36 @@
-class Productos:
+class Producto:
+    
+    
+    idProducto = 0 
+    def __init__(self,  nombre, categoria, precio, stock):
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio = precio
+        self.stock = stock
+	Producto.idProducto += 1
+	self.id = Producto.idProducto
 	
-	def __init__(self, codigoUnico,nombre, Categoria,precio,stock,cantidad, tipoMoviento, fecha):
-		self.codigoUnico = codigoUnico
-		self.nombre = nombre
-		self.categoria = categoria
-		self.precio = precio
-		self.stock = stock
-		self.cantidad = cantidad
-		self.tipoMoviento = tipoMoviento
-		self.fecha = fecha
-		Productos.idProducto += 1
-		self.id = Productos.idProducto
-	
-	#Getters
-	def getCodigoUnico(self):
-		return self.codigoUnico
-	
-	def getNombre(self):
-		return self.nombre
-	
-	def getCategoria(self):
-		return self.categoria
-	
-	def getPrecio(self):
-		return self.precio
-	
-	def getStock(self):
-		self.stock
-	
-	def getCantidad(self):
-		return self.cantidad
-	
-	def getTipoMoviento(self):
-		return self.tipoMoviento
-	
-	def getFecha(self):
-		self.fecha
-		
-	#Setters
-	def setCodigoUnico(self, codigoUnico):
-		self.codigoUnico = codigoUnico
-	
-	def setNombre(self, nombre):
-		self.nombre = nombre
-	
-	def setCategoria(self, categoria):
-		self.categoria = categoria
-	
-	def setPrecio(self, precio):
-		self.precio = precio
-	
-	def setStock(self, stock):
-		self.stock = stock
-		
-	def setCantidad(self, cantidad):
-		self.cantidad = cantidad
-	
-	def setTipoMoviento(self, tipoMoviento):
-		self.tipoMoviento = tipoMoviento
-		
-	def setPrecio(self, precio):
-		self.precio = precio
-		
-	
-	def mostrarDatos(self):
-		return f" Codigo : {self.codigoUnico} Nombre: {self.nombre} Categoria: {self.categoria} Precio: {self.precio} Stock: {self.stock} Cantidad: {self.cantidad} TipoMoviento: {self.tipoMoviento} Fecha: {self.fecha}"
+#Getters
+    def getNombre(self):
+        return self.nombre
+
+    def getCategoria(self):
+        return self.categoria
+
+    def getPrecio(self):
+        return self.precio
+
+    def getStock(self):
+        return self.stock
+#Setters
+    def setNombre(self, nombre):
+        self.nombre = nombre
+
+    def setCategoria(self, categoria):
+        self.categoria = categoria
+
+    def setPrecio(self, precio):
+       self.precio = precio
+    
+    def setStock(self, stock):
+        self.stock = stock
