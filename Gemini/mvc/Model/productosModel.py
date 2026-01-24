a3d058e9380d6509ac3cea3f8ab22b05e87b52fa
Davox1095 <davidmorag1095@gmail.com>
2026-01-24 13:57:31 -0600

    asd

--- Gemini/mvc/Model/productosModel.py
+++ Gemini/mvc/Model/productosModel.py
@@ -5,3 +5,35 @@ class Producto:
         self.categoria = categoria
         self.precio = precio
         self.stock = stock
+
+# GETS
+def getId(self):
+	return self.id
+
+def getNombre(self):
+	return self.nombre
+
+def getCategoria(self):
+	return self.categoria
+
+def getPrecio(self):
+	return self.precio
+
+def getStock(self):
+	return self.stock
+
+# SETS
+def setId(self, id):
+	self.id = id
+
+def setNombre(self, nombre):
+	self.nombre = nombre
+
+def setCategoria(self, categoria):
+	self.categoria = categoria
+
+def setPrecio(self, precio):
+	self.precio = precio
+  
+def setStock(self, stock):
+	self.stock = stock
