0d1f0138eb2db7756005d056f1018202e6f9ef1f
Davox1095 <davidmorag1095@gmail.com>
2026-01-24 12:14:43 -0600

    asd

--- Laboratorio #2/mvc/View/productosView.py
+++ Laboratorio #2/mvc/View/productosView.py
@@ -1,8 +1,11 @@
 from Controller import productosController as controller
 
 def menuProductos():
-    print("1.Registrar")
-    print("2.Listar")
-    print("3.Modificar")
-    print("4.Eliminar")
-
+	print("""
+=== Sistema de Bodega ===
+1. Registrar
+2. Listar
+3. Modificar
+4. Eliminar
+""")
+	
