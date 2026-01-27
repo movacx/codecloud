b2e98de4221c4f17b8dbbfba8a00fcaecb4135c1
Jos-2006 <camposjoseph136@gmail.com>
2026-01-27 10:52:51 -0600

    a

--- Quiz_2/MVC/Data/baseReserva.py
+++ Quiz_2/MVC/Data/baseReserva.py
@@ -1,13 +1,79 @@
-import  os
+import os
 import sys
 import csv
 
-sys.stdout.reconfigure(enconding = "utf-8")
+#from Model.reservaModel import ReservaModel
 
-import csv
+sys.stdout.reconfigure(encoding = "utf-8")
 
-BASE_DIR = os.path.dirname(os.path.abspath((__file__)))
+dir_name = os.path.dirname(os.path.abspath((__file__)))
+ARCHIVO = os.path.join(dir_name,'csv','baseReserva.csv')
 
-ARCHIVO = os.path.join(BASE_DIR, 'csv', 'baseReserva.csv')
+#--------------------------------------------------------------------------------------------------#
 
-def
+def verificarUltimoId():
+	if not os.path.exists(ARCHIVO):
+		return 0
+	
+	ultimoId = 0
+	
+	with open(ARCHIVO,'r', newline='',encoding='utf-8') as archivo_para_leer:
+		reader = csv.reader(archivo_para_leer)
+		
+		for lista in reader:
+			if lista:
+				if int(lista[0]) > ultimoId:
+					ultimoId = int(lista[0])
+	return ultimoId
+
+#--------------------------------------------------------------------------------------------------#
+
+def agregarListado(ReservaModel):
+	id_quemado = verificarUltimoId()
+	ultimoId = id_quemado + 1
+	
+	ReservaModel.setId(ultimoId)
+	
+	with open(ARCHIVO, 'a', newline='',encoding='utf-8') as archivo_para_agregar:
+		writer = csv.writer(archivo_para_agregar)
+		writer.writerow(ReservaModel.importarToCsv())
+
+#--------------------------------------------------------------------------------------------------#
+
+def listarTodos():
+	if not os.path.exists(ARCHIVO):
+		return []
+	
+	copiarLista = []
+	with open(ARCHIVO,'r',newline='',encoding='utf-8') as archivo_para_leer:
+		reader = csv.reader(archivo_para_leer)
+		for lista in reader:
+			if lista:
+				copiarLista.append(lista)
+				
+	return copiarLista
+
+#--------------------------------------------------------------------------------------------------#
+
+def searchName(nombre):
+	if not os.path.exists(ARCHIVO):
+		return []
+	
+	nombreEncontrado = []
+	
+	with open(ARCHIVO, 'r', newline='',encoding='utf-8') as archivo_para_leer:
+		reader = csv.reader(archivo_para_leer)
+		
+		for lista in reader:
+			if lista:
+				nombre_en_csv = lista[2].strip().lower()
+				nombre_a_buscar = nombre.strip().lowe()
+				
+				if nombre_en_csv == nombre_a_buscar:
+					nombreEncontrado.append(lista)
+					
+	return nombreEncontrado
+		
+#--------------------------------------------------------------------------------------------------#
+
+def modificarLista(id, ReservaModel)
