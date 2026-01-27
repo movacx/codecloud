1684367603e20afc9f55dc2f9764e12351de3097
Davox1095 <davidmorag1095@gmail.com>
2026-01-27 14:26:06 -0600

    asd

--- Quiz_2/MVC/Controller/habitacionesController.py
+++ Quiz_2/MVC/Controller/habitacionesController.py
@@ -1,17 +1,17 @@
 import View.habitacionesView as vista
 import Data.baseHabitacion as baseHabitacion
 from Model.HabitacionModel import Habitacion
-class habitacionController:
+class HabitacionController:
 	
 	def __init__(self):
 		pass
 	
-	def registrarHabitacion(numero, tipo, precio, estado):
+	def registrarHabitacion(self,numero, tipo, precio, estado):
 		nuevaHabitacion = Habitacion(0, numero, tipo, precio, estado)
 		baseHabitacion.agregarListado(nuevaHabitacion)
 		
 		
-	def listarHabitaciones():
+	def listarHabitaciones(self):
 		
 		mostrarTodos = baseHabitacion.listarHabitaciones()
 		if not mostrarTodos:
---
+++ Quiz_2/MVC/testMenuHabitaciones.py
@@ -0,0 +1,33 @@
+from Controller.habitacionesController import HabitacionController
+
+def main():
+	baseHabitaciones = HabitacionController()
+	
+	cerrar = True
+	while cerrar:
+		print("1.agregar/2.Listar/3.Buscar/4.Cambiar estado/5.Ordenado por precio")
+		op = int(input("Input: "))
+		if op ==1:
+			numero = input("Numero: " )
+			tipo = input("Tipo de habitacion: ")
+			precio: input("Precio: ")
+			estado: input("Estado: ")
+			baseHabitaciones.registrarHabitacion(numero, tipo, precio, estado)
+		elif op == 2:
+			pass
+		elif op ==3:
+			pass
+		elif op == 4:
+			pass
+		elif op ==5:
+			pass
+		elif op == 0:
+			cerrar = False
+			break
+		else:
+			vista,mostrarMensaje("Opcion invalida")
+			
+if __name__ == "__main__":
+	main()
+		
+		
