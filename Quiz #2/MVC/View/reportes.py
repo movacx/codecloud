c38a7c4f840cb1846270483cdc4ee4d82e513111
movacx <fabianbeita36@gmail.com>
2026-01-28 13:51:52 -0600

    d

--- Quiz #2/MVC/View/reportes.py
+++
@@ -1,4 +0,0 @@
-def reporte_reservas_activas(reservas):
-    print(" Reservaciones activas")
-    for rows in reservas:
-        print(rows.mostrar_info())
--- Quiz #2/MVC/View/reservaView.py
+++
@@ -1,9 +0,0 @@
-def mostrarMensaje(mensaje):
-    print(mensaje)
-
-def mostrarReservas(reservas):
-    if len(reservas) == 0:
-        print("No hay reservas registradas")
-    else:
-        for rows in reservas:
-            print(rows.mostrar_info())
