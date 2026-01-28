6e2506177adbfa2b7d46b1571dc9f88eb518e57b
Davox1095 <davidmorag1095@gmail.com>
2026-01-28 14:15:48 -0600

    asda

--- Quiz #2/MVC/Data/baseReserva.py
+++ Quiz #2/MVC/Data/baseReserva.py
@@ -1,24 +1,157 @@
+import os
+import sys
 import csv
+
 from Model.reservaModel import Reserva
 
-ARCHIVO = "Data/csv/reservas.csv"
+sys.stdout.reconfigure(encoding = "utf-8")
 
-def guardar_reservas(lista_reservas):
-    with open(ARCHIVO, "w", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        for rows in lista_reservas:
-            writer.writerow([rows.id_reserva,rows.numero_habitacion,rows.id_huesped,rows.fecha_entrada,rows.fecha_salida])
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ARCHIVO = os.path.join(BASE_DIR, "csv", "reservas.csv")
 
-def cargar_reservas():
-    reservas = []
-    try:
-        with open(ARCHIVO, "r", encoding="utf-8") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if row:
-                    reservas.append(
-                        Reserva(row[1],row[2],row[3],row[4],row[0])
-                    )
-    except FileNotFoundError:
-        pass
-    return reservas
+#ValidarUltimoID
+def validarUltimoId():
+	if not os.path.exists(ARCHIVO):
+		return 0
+	
+	ultimoId = 0
+	
+	with open(ARCHIVO, "r", newline="", encoding="utf-8") as archivoParaLeer:
+		reader = csv.reader(archivoParaLeer)
+
+		for item in reader:
+			if item:
+				if int(item[0]) > ultimoId:
+					ultimoId = int(item[0])
+			
+	return ultimoId	
+#Registrar reservaciones
+def registrarReservacion(Reserva):
+	idQuemado = validarUltimoId()
+	ultimoId = idQuemado + 1
+	
+	Reserva.setIde(ultimoId)
+	
+	with open(ARCHIVO, "a", newline="", encoding = "utf-8") as archivoParaGuardar:
+		writer = csv.writer(archivoParaGuardar)
+		writer.writerow(Reserva.importarToCsv())
+		
+
+#Listar habitaciones
+def listarHabitaciones():
+	if not os.path.exists(ARCHIVO):
+		return 0 
+	listaVacia = [ ]
+	
+	with open(ARCHIVO, "r", newline= "", encoding= "utf-8") as archivoParaLeer:
+		reader = csv.reader(archivoParaLeer)
+		
+		for item in reader:
+			listaVacia.append(item)
+			
+	return listaVacia
+
+#Buscar por número
+def buscarHabitacionId(id):
+	if not os.path.exists(ARCHIVO):
+		return 0
+	
+	encontrado = False 
+	habitacionEncontrada = [ ]
+	with open(ARCHIVO, "r", newline="", encoding= "utf-8") as archivoParaLeer:
+		reader = csv.reader(archivoParaLeer)
+		
+		for item in reader:
+			if item:
+				if int(item[0]) == int(id):
+					encontrado = True 
+					habitacionEncontrada.append(item)
+	return habitacionEncontrada
+
+#Cambiar estado “Disponible/Ocupada”
+def modificar(id, estado): 
+	if not os.path.exists(ARCHIVO):
+		return []
+	
+	encontrado = False
+	arregloVacio = []
+	
+	with open(ARCHIVO,'r',newline='',encoding='utf-8') as archivoParaLeer:
+		reader = csv.reader(archivoParaLeer)
+		
+		for item in reader:
+			if item:
+				
+					if int(item[0]) == int(id):
+						item[4] = estado 
+						arregloVacio.append(item)
+						encontrado = True
+					else:
+						arregloVacio.append(item)
+													
+	if encontrado == True:		
+		with open(ARCHIVO,'w',newline='',encoding='utf-8') as archivoParaEscribir:
+			writer = csv.writer(archivoParaEscribir)
+			writer.writerows(arregloVacio)
+		return encontrado
+	else:
+		return encontrado
+		
+#Ordenar por precio (usar Bubble Sort o sort())
+def ordenarPrecio():
+    listaHabitaciones = listarHabitaciones()
+    listaTemporalOrden = []
+
+    for item in listaHabitaciones:
+        listaTemporalOrden.append([
+            float(item[3]),  # precio primero
+            item[0],
+            item[1],
+            item[2],
+            item[4]
+        ]) 
+
+    listaTemporalOrden.sort()
+
+    listaHabitacionesOrdenadas = []
+
+    for item in listaTemporalOrden:
+        listaHabitacionesOrdenadas.append([
+            item[1],
+            item[2],
+            item[3],
+            item[0],
+            item[4]
+        ])
+
+    return listaHabitacionesOrdenadas
+
+def eliminarHabitacion(idHabitacion):
+	if not os.path.exists(ARCHIVO):
+		return [ ]
+	
+	arregloVacio = [ ]
+	encontrado = False
+	with open(ARCHIVO, "r", newline= "", encoding = "utf-8") as archivoParaLeer:
+		reader = csv.reader(archivoParaLeer)
+		
+		for item in reader:
+			if int(item[0]) != int(idHabitacion):
+				arregloVacio.append(item)
+				encontrado = False
+			else:
+				encontrado = True
+	if encontrado == True:
+		with open(ARCHIVO,"w", newline="", encoding = "utf-8") as archivoParaEscribir:
+			writer = csv.writer(archivoParaEscribir)
+			writer.writerows(arregloVacio)
+			return encontrado
+	else:
+		return encontrado
+					
+
+
+
+	
+	
+	
--- Quiz #2/MVC/Model/reservaModel.py
+++ Quiz #2/MVC/Model/reservaModel.py
@@ -8,5 +8,9 @@ class Reserva(HuespedModel):
         self.fechaEntrada = fechaEntrada
         self.fechaSalida = fechaSalida
 
+
+    def importarToCsv(self):
+	    return ([self.id, self.numeroHabitacion, super().self.idHuesped, self.fechaEntrada, self.fechaSalida])
+    
     def mostrarInfo(self):
         return f"Id reservacion: {self.id} Reservacion numero: {self.numeroHabitacion} Huesped Id: {self.idHuesped} Fecha de entrada: {self.fechaEntrada} Fecha de salida:  {self.fechaSalida}"
