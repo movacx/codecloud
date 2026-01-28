b11582621c7ab865d708ac335320230fb7e2b1f3
movacx <fabianbeita36@gmail.com>
2026-01-28 14:23:03 -0600

    d

--- Quiz #2/MVC/Model/reservaModel.py
+++ Quiz #2/MVC/Model/reservaModel.py
@@ -1,17 +1,12 @@
-from Model.huespedModel import HuespedModel
-class Reserva(HuespedModel):
-    contador_id = 1
-    def __init__(self,id,numeroHabitacion,idHuesped, fechaEntrada, fechaSalida):
-        super().__init__(idHuesped)
+# No heredamos de HuespedModel, son cosas distintas
+class Reserva: 
+    def __init__(self, id, numeroHabitacion, idHuesped, fechaEntrada, fechaSalida):
         self.id = id
         self.numeroHabitacion = numeroHabitacion
+        self.idHuesped = idHuesped # Guardamos el ID como una variable normal
         self.fechaEntrada = fechaEntrada
         self.fechaSalida = fechaSalida
 
-
     def importarToCsv(self):
-	    return ([self.id, self.numeroHabitacion, super().self.idHuesped, self.fechaEntrada, self.fechaSalida])
-    
-
-    # def mostrarDatos(self):
-    #     return f"[ID: {self.id}] {super().mostrarDatos()} | Edad: {self.edad} | Grado: {self.grado}"
+        # Es mucho mas simple
+        return ([self.id, self.numeroHabitacion, self.idHuesped, self.fechaEntrada, self.fechaSalida])
