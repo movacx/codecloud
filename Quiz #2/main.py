4223fa995ae8d17b8ac41ebd278b71de705ee39e
movacx <fabianbeita36@gmail.com>
2026-01-28 13:13:32 -0600

    dd

--- Quiz #2/main.py
+++ Quiz #2/main.py
@@ -23,49 +23,21 @@ def main():
             """)
 
         op = input("Seleccione: ")
-#numero, tipo, precio, estado
         if op == "1":
-            menu_habitaciones(hCtrl)
+            pass
 
         elif op == "2":
-            menu_habitaciones(hCtrl)
+            pass
 
         elif op == "3":
-            menu_habitaciones(hCtrl)
+            pass
 
         elif op == "4":
-            menu_habitaciones(hCtrl)
+            pass
 
         elif op == "5":
             break
 
-def menu_habitaciones(ctrl):
-    print("""
-            --- Habitaciones ---
-            1. Registrar
-            2. Listar
-            3. Buscar
-            4. Cambiar Estado
-            5. Ordenar por Precio
-            6. o Cualquier tecla para regresar
-            """)
-    op = input("Seleccione: ")
-
-    if op == "1":
-        numero = input("Número: ")
-        tipo = input("Tipo: ")
-        precio = input("Precio: ")
-        estado = input("Estado: ")
-        ctrl.registrar(numero, tipo, precio, estado)
-
-    elif op == "2":
-        ctrl.listar()
-        
-    elif op == "3":
-        ctrl.listar()
-
-    elif op == "4":
-        ctrl.listar()
 
 
    
