38a2960415bd9ee1cf2bd59c8e332b3c80ee6f7f
Jos-2006 <camposjoseph136@gmail.com>
2026-01-28 20:01:57 -0600

    a

---
+++ Joseph/mvc/model/gastosModel.py
@@ -0,0 +1,16 @@
+class GastoModel:
+
+    def __init__(self, id, descripcion, monto, categoria, fecha)
+    self.id=id
+    self.descripcion=descripcion
+    self.monto=monto
+    self.categoria.categoria
+    self.fecha=fecha
+    
+    setId(self, id):   
+       self.id=id
+    
+    getId(self):
+        return self.id
+    def importarToCsv(self):
+        return([self.id, descripcion, monto, categoria, fecha])
