d419fb47be2647295e00557482b52464683ffe67
Davox1095 <davidmorag1095@gmail.com>
2026-01-29 17:05:25 -0600

    asda

--- PRACTICA EXAMEN/mvc/data/baseReserva.py
+++ PRACTICA EXAMEN/mvc/data/baseReserva.py
@@ -1,1 +1,39 @@
-asda
+import sys
+import os
+import csv
+from Model.reservaModel import Reserva
+sys.stdout.reconfigure(encoding = "utf-8")
+
+BASE_DIR = os.path.dirname(__file__)
+ARCHIVO = os.path.join(BASE_DIR, "csv", "reserva.csv")
+
+#Validar ultimoId
+def validarUltimoId():
+	if not os.path.eexists(ARCHIVO):
+		return 0 
+	ultimoId = 0
+	with open(ARCHIVO, "r", nenewline= "", encoding= "utf-8") as archivoParaLeer:
+		reader = csv.reader(archivoParaLeer)
+		
+		for item in reader:
+			if int(item[0]) > ultimoId:
+				ultimoId = int(item[0])
+				
+	return ultimoId
+
+#---------------------------------------------------------------------------------------------#
+#Registrar reserva
+def registrarReserva(Reserva):
+	idQuemado = validarUltimoId()
+	ultimoId = idQuemado +1 
+	
+	Reserva.setId(ultimoId)
+	
+	with open(ARCHIVO, "a" , newline="", enconding = "utf-8") as archivoParaGuardar
+	writer = csv.writer(archivoParaGuardar)
+	writer.writerows(archivoParaGuardar)
+	
+	
+	
+				
+	
--- PRACTICA EXAMEN/mvc/model/reservaModel.py
+++ PRACTICA EXAMEN/mvc/model/reservaModel.py
@@ -1,1 +1,21 @@
-asda
+class Reserva:
+	def __init__(self,id, numeroCancha, idSocio, fecha, horaInicio, horaFin):
+		self. numeroCancha = numenumeroCancha
+		self.idSocio = idSocio
+		self.fecha = fecha
+		self.horaInicio = horaInicio
+		self.horaFin = horaFin
+#---------------------------------------------------------------------------------------------#		
+	#Get y set ID
+	def getId(self):
+		return self.id
+	
+	def setId(self, id):
+		self.id = id
+#---------------------------------------------------------------------------------------------#			
+#Metodo importToCsv
+	def importToCsv(self):
+		return ([self.numeroCancha, self.idSocio, self.fecha, self.horaInicio, self.horaFin])
+	
+	
+		
