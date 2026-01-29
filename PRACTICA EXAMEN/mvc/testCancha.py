9ef03fb13f33931059a09837a230156e90f25e62
movacx <fabianbeita36@gmail.com>
2026-01-29 17:52:49 -0600

    d

---
+++ PRACTICA EXAMEN/mvc/testCancha.py
@@ -0,0 +1,18 @@
+
+
+def main():
+    
+    while True:
+        opcion= int(input("1 registrar n/ 2 listar 3 eliminar 4 total gastado" ))
+            pass
+        elif opcion== 2:
+            pass
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            pass
+                
+            
+        
+if __name__=="__main__":
+    main()
