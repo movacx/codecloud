89f56023212b997e37d691158859a985114c3056
movacx <fabianbeita36@gmail.com>
2026-01-29 10:43:46 -0600

    d

---
+++ Actividades/Fabian/mvc/main.py
@@ -0,0 +1,19 @@
+from controller.gastoController import GastoController
+
+def main():
+    manejoGasto=GastoController()
+    
+    while True:
+        opcion= int(input("1 registrar n/ 2 listar"))
+        if opcion ==1:
+            descripcion= input("Descripcio: ")
+            monto= int(input("monto: "))
+            categoria= input("categoria: ")
+            fecha= input("fecha: ")
+            manejoGasto.registrarGasto(descripcion, monto, categoria, fecha)
+            pass
+        elif opcion== 2:
+            manejoGasto.listar()
+        
+if __name__=="__main__":
+    main()
