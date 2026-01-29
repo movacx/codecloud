2ffa4e720a25d68af521bd2ebd00d4a4914f03b3
Jos-2006 <camposjoseph136@gmail.com>
2026-01-28 20:44:47 -0600

    a

--- Joseph/mvc/data/gastoData.py
+++ Joseph/mvc/data/gastoData.py
@@ -1,3 +1,16 @@
 import os
 import sys
-import stout
+import csv
+
+dir_data= os.path.dirname(__file__)#MVC/DATA/gastoData.py
+ARCHIVO=os.path.join(dir_data,"csv","RegistroGasto.csv")
+#dir_data/csv/registroGasto.csv
+def validarUltimoId():
+    if not os.path.exists(ARCHIVO):
+        return 0
+    
+    ultimoId=0
+    with open(ARCHIVO,newline="",encoding="utf-8")as archivoParaLeer:
+        reader=csv.reader(archivoParaLeer)
+    
+    
