b665513aea221bf4484e2d799cacb2424990d443
movacx <fabianbeita36@gmail.com>
2026-01-29 20:01:27 -0600

    d

--- PRACTICA EXAMEN/mvc/view/reservaView.py
+++ PRACTICA EXAMEN/mvc/view/reservaView.py
@@ -2,7 +2,7 @@ def mostrarMensaje(mensaje):
     print(mensaje)
 
 def imprimirListados(arreglo):
-    print(f'{'numero':<8}|{'tipo':<15}|{'tarifa':<10}|{'estado':<12}')
+    print(f"{'numero':<8}|{'tipo':<15}|{'tarifa':<10}|{'estado':<12}")
     print('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
     for items in arreglo:
         print(f'{items[0]:<8}|{items[1]:<15}|{items[2]:<10}|{items[3]:<12}')
