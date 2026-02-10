f60d6d606944e49af97a730a5bbc6acffec81a4f
Jos-2006 <camposjoseph136@gmail.com>
2026-02-10 14:16:47 -0600

    a

--- HOTEL GUI/View/reservasGUI.py
+++ HOTEL GUI/View/reservasGUI.py
@@ -1,22 +1,61 @@
-import tkinter as tk
+import tkinter as tkGUI
 
-class ReservasGUI:
-    def __init__(self, baseMainGUI):
-        self.baseMainGUI = FormularioGUI.Toplevel(baseMainGUI)
-        self.baseMainGUI.title("Reservas Base")
-        self.baseMainGUI.geometry("900x950")
+# from tkinter import ttkAuxiliar
+
+# from tkinter import messagebox
+
+ 
+
+ 
+
+class reservasGUI:
+
+    
+
+    def __init__(self, baseGUI):
+
+        self.baseGUI = baseGUI
+
+        self.baseGUI.title("Sistema en habitaciones")
+
+        self.baseGUI.geometry("700x400")
+
         
-        self.frame_form=tkGUI.Frame(baseGUI)
-        self.frame+form.grid(row=0, column=0)
+
+        #Paneles
+
+        #self.frame_form = tk(Tramas, inputs labels, textareas, choose)
+
+        self.frame_form = tkGUI.Frame(baseGUI )
+
+        self.frame_form.grid(row=0, column=0)
+
         
+
+        
+
         self.formularioCrear()
+
+        #Siempre creen funciones como las que hicimso en la vista
+
         
+
     def formularioCrear(self):
-        tkGUI.Label(self.frame_form, text="Etiqueta").grind(row = 0, column=0)
-        self.campoText=tkGUI.Entry(self.frame_form)
-        self.campoText.grid(row=0, column =1)
-  
-if __name__ == "__main__":
-    baseGUI= tkGUI.Tk()
-    app= ReservasGUI(baseGUI)
-    baseGUI(mainloop())
+
+        tkGUI.Label(self.frame_form, text ="Etiqueta" ).grid(row=0, column=0)
+
+        self.campoText= tkGUI.Entry(self.frame_form)
+
+        self.campoText.grid(row=0, column=1)
+
+        
+
+ 
+
+if __name__ == "__main__":     
+
+     baseGUI = tkGUI.Tk()
+
+     app = reservasGUI(baseGUI)
+
+     baseGUI.mainloop()
