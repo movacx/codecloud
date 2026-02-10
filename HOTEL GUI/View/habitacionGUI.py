2ee3bdcabf8420751b2d721e9c1272310ed0ed1a
movacx <fabianbeita36@gmail.com>
2026-02-10 14:25:29 -0600

    asdasd

--- HOTEL GUI/View/habitacionGUI.py
+++ HOTEL GUI/View/habitacionGUI.py
@@ -1,36 +1,25 @@
-from tkinter import tkGUI
-from tkinter import ttkAuxiliar
+import tkinter as tk # Lo estándar es usar 'tk'
+from tkinter import ttk
 from tkinter import messagebox
 
 class HabitacionGUI:
     def __init__(self, baseMainGUI):
+        self.ventana_hija = tk.Toplevel(baseMainGUI)
+        self.ventana_hija.title("Sistema Habitación")
+        self.ventana_hija.geometry("300x250")
         
-        self.baseMainGUI = tk.Toplevel(baseMainGUI)
-        self.baseMainGUI.title("Sistema Habitación")
-        self.baseMainGUI.geometry("300x250")
-        
-        #Paneles
-        #self.frame_form = tk(Tramas, inputs labels, textareas, choose)
-        self.frame_form = tkGUI.Frame(baseGUI )
-        self.frame_form.grid(row=0, column=0)
-        
+        self.frame_form = tk.Frame(self.ventana_hija)
+        self.frame_form.grid(row=0, column=0, padx=10, pady=10)
         
         self.formularioCrear()
-        #Siempre creen funciones como las que hicimso en la vista
         
     def formularioCrear(self):
-        tkGUI.Label(self.frame_form, text ="Etiqueta" ).grid(row=0, column=0)
-        self.campoText= tkGUI.Entry(self.frame_form)
-        self.campoText.grid(row=0, column=1)
-		
-    def formularioCrear(self):
-        tkGUI.Label(self.frame_form, text ="Etiqueta" ).grid(row=0, column=0)
-        self.campoText= tkGUI.Entry(self.frame_form)
+        tk.Label(self.frame_form, text="Etiqueta").grid(row=0, column=0)
+        self.campoText = tk.Entry(self.frame_form)
         self.campoText.grid(row=0, column=1)
         
 if __name__ == "__main__":     
-     baseGUI = tkGUI.Tk()
-     app = HabitacionGUI(baseGUI)
-     baseGUI.mainloop()       
- 
- 
+    root = tk.Tk()
+    root.title("Ventana Principal")
+    app = HabitacionGUI(root)
+    root.mainloop()
