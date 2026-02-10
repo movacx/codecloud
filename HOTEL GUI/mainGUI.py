c554a3df594af5987a9f48b820ac76b5bbb2687c
movacx <fabianbeita36@gmail.com>
2026-02-10 11:43:01 -0600

    asdasd

--- HOTEL GUI/mainGUI.py
+++ HOTEL GUI/mainGUI.py
@@ -5,5 +5,16 @@ ventanaPrincipal = tkGUI.Tk()
 ventanaPrincipal.title('Sistema Costamar')
 
 ventanaPrincipal.geometry('1060x780')
+ventanaPrincipal.configure(bg='blue')
+
+contenedor = tkGUI.Frame(ventanaPrincipal, padx = 10, pady = 10)
+contenedor.place(anchor='center',relx = 0.5, rely = 0.5)
+
+tkGUI.Label(contenedor, text = 'Sistema Costa Mar', font = ('Arial',14), width = 30).grid(row=0,column=0, columnspan = 2)
+tkGUI.Button(contenedor, text = 'Habitacion', font = ('Arial',14), width = 30).grid(row=1,column=0, sticky = 'w', ipady = 10)
+tkGUI.Button(contenedor, text = 'Huespedes', font = ('Arial',14), width = 30).grid(row=2,column=0, sticky = 'w', ipady = 10)
+tkGUI.Button(contenedor, text = 'Reserva', font = ('Arial',14), width = 30).grid(row=1,column=1, sticky = 'w', ipady = 10)
+tkGUI.Button(contenedor, text = 'Reportes', font = ('Arial',14), width = 30).grid(row=2,column=1, sticky = 'w', ipady = 10)
+
 
 ventanaPrincipal.mainloop()
