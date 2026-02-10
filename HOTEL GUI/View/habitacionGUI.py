bd8f7b1d81d6f51c969456827801fa293bc91ef6
Davox1095 <davidmorag1095@gmail.com>
2026-02-10 14:21:14 -0600

    asda

--- HOTEL GUI/View/habitacionGUI.py
+++ HOTEL GUI/View/habitacionGUI.py
@@ -1,8 +1,36 @@
-import tkinter as tk 
+from tkinter import tkGUI
+from tkinter import ttkAuxiliar
+from tkinter import messagebox
 
 class HabitacionGUI:
     def __init__(self, baseMainGUI):
         
         self.baseMainGUI = tk.Toplevel(baseMainGUI)
-        self.baseMainGUI.title("Habitacion Base")
+        self.baseMainGUI.title("Sistema Habitación")
         self.baseMainGUI.geometry("300x250")
+        
+        #Paneles
+        #self.frame_form = tk(Tramas, inputs labels, textareas, choose)
+        self.frame_form = tkGUI.Frame(baseGUI )
+        self.frame_form.grid(row=0, column=0)
+        
+        
+        self.formularioCrear()
+        #Siempre creen funciones como las que hicimso en la vista
+        
+        def formularioCrear(self):
+		tkGUI.Label(self.frame_form, text ="Etiqueta" ).grid(row=0, column=0)
+		self.campoText= tkGUI.Entry(self.frame_form)
+		self.campoText.grid(row=0, column=1)
+		
+	def formularioCrear(self):
+        tkGUI.Label(self.frame_form, text ="Etiqueta" ).grid(row=0, column=0)
+        self.campoText= tkGUI.Entry(self.frame_form)
+        self.campoText.grid(row=0, column=1)
+        
+if __name__ == "__main__":     
+     baseGUI = tkGUI.Tk()
+     app = HabitacionesGUI(baseGUI)
+     baseGUI.mainloop()       
+ 
+ 
