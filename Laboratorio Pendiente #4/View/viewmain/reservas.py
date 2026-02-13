9354e8ea2c0aa74e4b0d03eaa5519419dd08f621
movacx <fabianbeita36@gmail.com>
2026-02-13 15:40:02 -0600

    asdasd

--- Laboratorio Pendiente #4/View/viewmain/reservas.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-class ReservaGUI:
-    def __init__(self, mainHuesped)
-    self.baseMainGUI = FormularioGUI.Toplevel(baseMainGUI)
-        self.baseMainGUI.title("Reserva Base")
-        self.baseMainGUI.geometry("900x950")
