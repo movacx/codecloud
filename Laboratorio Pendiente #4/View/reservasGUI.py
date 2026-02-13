33e8e91f23dd39f9ed7979cfa7cdefad2f0a89d5
Jos-2006 <camposjoseph136@gmail.com>
2026-02-13 16:45:36 -0600

    a

--- Laboratorio Pendiente #4/View/reservasGUI.py
+++ Laboratorio Pendiente #4/View/reservasGUI.py
@@ -33,3 +33,14 @@ class ReservaGUI:
 
         self.btn_confirmar = tk.Button(self.frame_contenedor, text="Confirmar Reservacion", bg="#FF9800", fg="white")
         self.btn_confirmar.grid(row=5, columnspan=2, pady=15, ipadx=10)
+        
+        
+    def table(self):
+        self.columnas = ["id", "numeroHabitacion", "idHuesped", "fechaEntrada", "fechaSalida",]
+        self.tabla = ttk.Treeview(self.contenedor, columns= self.columnas, show="headings" )
+        
+
+        for items in self.columnas:
+            self.tabla.heading(items, text = items.capitalize())
+            self.tabla.column(items, width=100)
+        self.tabla.grid(row=5, column=0, columnspan=3,sticky='nwse')
