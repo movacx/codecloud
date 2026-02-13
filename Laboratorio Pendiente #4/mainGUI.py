75227b78ad5eae0f9585913bd8629b5b75535ba4
movacx <fabianbeita36@gmail.com>
2026-02-13 12:47:05 -0600

    s

--- Laboratorio Pendiente #4/mainGUI.py
+++ Laboratorio Pendiente #4/mainGUI.py
@@ -1,39 +1,22 @@
 import tkinter as tkGUI
-# Asegúrate de que estas importaciones funcionen en tu estructura de carpetas
-from View.huespedesGUI import HuespedGUI 
-from View.habitacionGUI import HabitacionGUI
-from View.reservasGUI import ReservaGUI
-
-class MainGUI:
-    def __init__(self, ventanaPrincipal):
-        self.ventanaPrincipal = ventanaPrincipal
-        self.ventanaPrincipal.title('Sistema Costamar')
-        self.ventanaPrincipal.geometry('1060x780')
-        self.ventanaPrincipal.configure(bg='skyblue')
+from Controller.fileHuespedController import HuespedController
 
 
-        self.contenedor = tkGUI.Frame(self.ventanaPrincipal, padx=20, pady=20, bg='white', highlightbackground="gray", highlightthickness=1)
-        self.contenedor.place(anchor='center', relx=0.5, rely=0.5)
+def main():
+    root = tkGUI.Tk()
+    root.title('Sistema Costamar')
+    root.geometry('1060x780')
+    root.configure(bg='skyblue')
+    app = HuespedController(root)
 
-        tkGUI.Button(self.contenedor, text='Habitación', font=('Arial', 14), width=25, 
-                     command=lambda: HabitacionGUI(self.ventanaPrincipal)).grid(row=0, column=0, padx=10, pady=10, ipady=10)
+    contenedor = tkGUI.Frame(root, padx=20, pady=20, bg='white', highlightbackground="gray", highlightthickness=1)
+    contenedor.place(anchor='center', relx=0.5, rely=0.5)
 
-
-        tkGUI.Button(self.contenedor, text='Reserva', font=('Arial', 14), width=25, 
-                     command=lambda: ReservaGUI(self.ventanaPrincipal)).grid(row=0, column=1, padx=10, pady=10, ipady=10)
-
-
-        tkGUI.Button(self.contenedor, text='Huéspedes', font=('Arial', 14), width=25, 
-                     command=lambda: HuespedGUI(self.ventanaPrincipal)).grid(row=1, column=0, padx=10, pady=10, ipady=10)
-
-
-        tkGUI.Button(self.contenedor, text='Reportes', font=('Arial', 14), width=25).grid(row=1, column=1, padx=10, pady=10, ipady=10)
+    tkGUI.Button(contenedor, text='Huéspedes', font=('Arial', 14), width=25).grid(row=1, column=0, padx=10, pady=10, ipady=10)
+    tkGUI.Button(contenedor, text='Reportes', font=('Arial', 14), width=25).grid(row=1, column=1, padx=10, pady=10, ipady=10)
+    root.mainloop()
 
 
-    def crear_menu(self):
-        pass
 
 if __name__ == "__main__":
-    root = tkGUI.Tk()
-    app = MainGUI(root)
-    root.mainloop()
+    main()
