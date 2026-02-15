8c3475f3e9eb80ec4bbb7846275244d364f9ef47
movacx <fabianbeita36@gmail.com>
2026-02-15 01:11:51 -0600

    asd

--- Laboratorio Pendiente #4/mainGUI.py
+++ Laboratorio Pendiente #4/mainGUI.py
@@ -1,4 +1,5 @@
 import tkinter as tkGUI
+from tkinter import scrolledtext
 from Controller.fileHuespedController import HuespedController
 from Controller.fileHabitacionController import HabitacionController
 from Controller.fileReportesController import ReportesController
@@ -6,36 +7,46 @@ from Controller.fileReportesController import ReportesController
 from View.fileReservasGUI import ReservaGUI
 
 def main():
+    try:
+        root = tkGUI.Tk()
+        root.geometry('600x300')
+        root.title('Condominios Costa Mar')
 
-    root = tkGUI.Tk()
-    root.geometry('600x300')
-    root.title('Condominios Costa Mar')
+        root.columnconfigure(0, weight = 0)
+        root.columnconfigure(1, weight = 1)
+        root.rowconfigure(0, weight = 1)
+        root.configure(bg = 'white')
+        
 
-    root.columnconfigure(0, weight = 0)
-    root.columnconfigure(1, weight = 1)
-    root.rowconfigure(0, weight = 1)
-    root.configure(bg = 'white')
+        contenedor = tkGUI.Frame(root, bg = "#4b4242")
+        contenedor.grid(row = 0, column = 0, sticky = 'ns')
 
-    contenedor = tkGUI.Frame(root)
-    contenedor.grid(row = 0, column = 0, sticky = 'ns')
-
-    contenedor.rowconfigure(0, weight = 1)
-    contenedor.rowconfigure(1, weight = 1)
-    contenedor.rowconfigure(2, weight = 1)
-    contenedor.rowconfigure(3, weight = 1)
-
-    tkGUI.Button(contenedor, text = 'Habitaciones', width = 10, command = lambda: HabitacionController(root)).grid(row = 0, column = 0, sticky = 'nswe' )
-    tkGUI.Button(contenedor, text = 'Huesped', width = 10, command = lambda: HuespedController(root)).grid(row = 1, column = 0, sticky = 'nswe') #Completado
-    tkGUI.Button(contenedor, text = 'Reservas', width = 10, command = lambda: ReservaGUI(root,None)).grid(row = 2, column = 0, sticky = 'nswe')  
-    tkGUI.Button(contenedor, text = 'Reportes', width = 10, command = lambda: ReportesController(root)).grid(row = 3, column = 0, sticky = 'nswe') #Completado
+        contenedor.rowconfigure(0, weight = 1)
+        contenedor.rowconfigure(1, weight = 1)
+        contenedor.rowconfigure(2, weight = 1)
+        contenedor.rowconfigure(3, weight = 1)
+        contenedor.rowconfigure(4, weight = 1)
+        contenedor.rowconfigure(5, weight = 1)
 
 
 
+        tkGUI.Button(contenedor, bd = 0, text = 'Habitaciones', width = 10, command = lambda: HabitacionController(root)).grid(row = 1, column = 0, sticky = 'nswe', pady = 5, padx = (5,5))
+        tkGUI.Button(contenedor, bd = 0, text = 'Huesped', width = 10, command = lambda: HuespedController(root)).grid(row = 2, column = 0, sticky = 'nswe', pady = 5, padx = (5,5)) #Completado
+        tkGUI.Button(contenedor, bd = 0, text = 'Reservas', width = 10, command = lambda: ReservaGUI(root,None)).grid(row = 3, column = 0, sticky = 'nswe', pady = 5, padx = (5,5))  
+        tkGUI.Button(contenedor, bd = 0, text = 'Reportes', width = 10, command = lambda: ReportesController(root)).grid(row = 4, column = 0, sticky = 'nswe', pady = 5, padx = (5,5)) #Completado
 
 
-    root.mainloop()
+        
+
+        root.mainloop()
+
+    except Exception as error:
+        print(F'Error {error}')
 
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as error:
+        print('Error {error}')
