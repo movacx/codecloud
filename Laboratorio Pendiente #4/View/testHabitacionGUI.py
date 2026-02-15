4308022f84157afe0baa5062317e5cf805ae9e45
movacx <fabianbeita36@gmail.com>
2026-02-15 10:44:48 -0600

    asd

---
+++ Laboratorio Pendiente #4/View/testHabitacionGUI.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+class HabitacionGUI:
+    def __init__(self, root, controller):
+        self.manejoController = controller
+        
+        self.ventanaHabitaciones = tk.Toplevel(root)
+        self.ventanaHabitaciones.geometry('2070x720')
+        self.ventanaHabitaciones.title('Registro Habitaciones')
+
+    def etiquetas(self):
+        
+        pass
+
+    def inputs(self):
+        pass
+
+    def buttons(self):
+        pass
+
+    def combobox(self):
+        pass
