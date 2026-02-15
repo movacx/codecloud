ea9801500d66e56ee78c3b9d71d53559eb6bfc92
movacx <fabianbeita36@gmail.com>
2026-02-15 15:48:50 -0600

    asd

--- Laboratorio Pendiente #4/Controller/testHabitacionController.py
+++ Laboratorio Pendiente #4/Controller/testHabitacionController.py
@@ -1,4 +1,5 @@
 from View.testHabitacionGUI import HabitacionGUI
+from Model.habitacionModel import HabitacionModel
 import Data.baseHabitacion as data
 
 class HabitacionController:
@@ -9,9 +10,71 @@ class HabitacionController:
     
     def savelog(self, nombreError):
         try:
+            
             data.guardarError(nombreError)
+            
         except Exception as error:
             print(f'No se pudo establecer la coneccion con log.txt >> data + controller = {error}')
             
+            #ide, numero, tipo, precio, estado
+            
+    def ultimoId(self):
+        try:
+            
+            ultimoId = data.validarUltimoId()
+            return ultimoId
+        
+        except Exception as error:
+            self.GUI.errorMessage(f'Hubo un error, valide logfile para mas informacion! {error}')
+            self.savelog(error)
+    
     def registrarHabitacion(self):
-        pass
+        try:
+            ultimaPosicion = self.ultimoId()
+            numeroHabitacion = self.GUI.numHabitacionTxt.get()
+            tipoHab = self.GUI.tipoHabitacioncbx.get()
+            precio = self.GUI.precioTxt.get()
+            estado = self.GUI.estadoHabitacioncbx.get()
+            
+            nuevaHabitacion = HabitacionModel(ultimaPosicion, numeroHabitacion, tipoHab, precio, estado)
+            exito = data.registrarHabitacion(nuevaHabitacion)
+            
+            
+            
+            if exito:
+                self.GUI.mostrarMensaje('Exito al guardar!')
+            else:
+                self.GUI.errorMessage('Hubo un error interno, valide el logfile')
+                
+            
+        except Exception as error:
+            self.GUI.errorMessage(f'No se pudo registrar la Habitacion. \nvalide logfile para mas informacion! {error}')
+            self.savelog(error)
+            
+    def imprimirHabitaciones(self):
+        try:
+
+            arreglo = data.listarHabitaciones()
+            self.GUI.listaHabitaciones.append(arreglo[1])
+            self.GUI.mostrarRegistros(arreglo)
+
+        except Exception as error:
+            self.savelog(error)
+            self.GUI.errorMessage(f'No se pudo mostrar la informacion. \nvalide logfile para saber mas! {error}')
+
+    def filtrado(self):
+        #-----------------------------------Recargar el filtrado de busqueda---------------------------#
+        registroHabitaciones = data.listarHabitaciones()
+        
+        listaHabitaciones = []
+        
+        for items in registroHabitaciones:
+            listaHabitaciones.append(items[2])
+            
+        return listaHabitaciones
+            
+    
+        
+        
+        
+        
--- Laboratorio Pendiente #4/View/testHabitacionGUI.py
+++ Laboratorio Pendiente #4/View/testHabitacionGUI.py
@@ -14,13 +14,37 @@ class HabitacionGUI:
         self.ventanaHabitacion.columnconfigure(0, weight = 0)
         self.ventanaHabitacion.columnconfigure(1, weight = 1)
         self.ventanaHabitacion.columnconfigure(2, weight = 0)
-        self.ventanaHabitacion.rowconfigure(9, weight = 1)
+        self.ventanaHabitacion.rowconfigure(14, weight = 1)
         
         self.labels()
         self.entry()
         self.combobox()
         self.botones()
         self.table()
+        
+
+        self.listaHabitacion = self.manejoController.filtrado()
+        self.busquedalista = ttk.Combobox(self.ventanaHabitacion, values = (self.listaHabitacion), state = 'readonly')
+        self.busquedalista.grid(row = 11, column = 1, sticky = 'nswe')
+        
+        
+        
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
       #numero, tipo, precio, estado  
     def separador(self, fila, columna):
         tk.Label(self.ventanaHabitacion, text = '').grid(row = fila, column = columna)
@@ -32,14 +56,16 @@ class HabitacionGUI:
         self.separador(3,0)
         tk.Label(self.ventanaHabitacion, text = 'Precio de Habitacion:').grid(row = 4, column = 0, sticky = 'w')
         self.separador(5,0)
-        tk.Label(self.ventanaHabitacion, text = 'Estado de Habitacion:').grid(row = 6, column = 0, sticky = 'w') 
+        tk.Label(self.ventanaHabitacion, text = 'Estado de Habitacion:').grid(row = 6, column = 0, sticky = 'w')
         self.separador(7,0)
+        tk.Label(self.ventanaHabitacion, text = 'Filtrar por Habitacion:').grid(row = 11, column = 0, sticky = 'w')
+        self.separador(8,0)
+        
+
+        
+        
         
     def combobox(self):
-        listaHabitaciones = ['101', '102', '103', '104', '105', '106', '107', '108', '109', '110']
-        
-        self.habitacioncbx = ttk.Combobox(self.ventanaHabitacion, values = (listaHabitaciones), state = 'readonly')
-        self.habitacioncbx.grid(row = 0, column = 1, sticky = 'nswe')
         
         listaTipoHabitacion = ['Sencilla', 'Doble']
         self.tipoHabitacioncbx = ttk.Combobox(self.ventanaHabitacion, values = (listaTipoHabitacion), state = 'readonly')
@@ -58,24 +84,34 @@ class HabitacionGUI:
         
     def botones(self):
         self.separador(5,0)
-        tk.Button(self.ventanaHabitacion, text = 'Guardar').grid(row = 6, column = 0, sticky = 'nswe')
-        tk.Button(self.ventanaHabitacion, text = 'Buscar').grid(row = 6, column = 1, sticky = 'nswe')
-        tk.Button(self.ventanaHabitacion, text = 'Modificar').grid(row = 6, column = 2, sticky = 'nswe')
-        tk.Button(self.ventanaHabitacion, text = 'Eliminar').grid(row = 7, column = 0, sticky = 'nswe')
-        tk.Button(self.ventanaHabitacion, text = 'Guardar').grid(row = 7, column = 1, sticky = 'nswe')
-        tk.Button(self.ventanaHabitacion, text = 'Limpiar').grid(row = 7, column = 2, sticky = 'nswe')
-        self.separador(8,0)
+        tk.Button(self.ventanaHabitacion, text = 'Guardar', command = lambda: self.manejoController.registrarHabitacion()).grid(row = 8, column = 0, sticky = 'nswe')
+        tk.Button(self.ventanaHabitacion, text = 'Mostrar Registros', command = lambda: self.manejoController.imprimirHabitaciones()).grid(row = 8, column = 1, sticky = 'nswe')
+        tk.Button(self.ventanaHabitacion, text = 'Modificar').grid(row = 8, column = 2, sticky = 'nswe')
+        tk.Button(self.ventanaHabitacion, text = 'Eliminar').grid(row = 9, column = 0, sticky = 'nswe')
+        tk.Button(self.ventanaHabitacion, text = 'Guardar').grid(row = 9, column = 1, sticky = 'nswe')
+        tk.Button(self.ventanaHabitacion, text = 'Limpiar').grid(row = 9, column = 2, sticky = 'nswe')
+        self.separador(10,0)
+        tk.Button(self.ventanaHabitacion, text = 'Filtrar').grid(row = 11, column = 2, sticky = 'nswe')
+        self.separador(12,0)
         
     def table(self):
         columnas = ['n°Habitacion','Tipo','Precio','Estado']
         self.tabla = ttk.Treeview(self.ventanaHabitacion, columns = columnas, height = 24, show = 'headings')
-        self.tabla.grid(row = 9, column = 0, columnspan = 4, sticky = 'nswe', padx = (5,5), pady = 5)
+        self.tabla.grid(row = 14, column = 0, columnspan = 4, sticky = 'nswe', padx = (5,5), pady = 5)
         for items in columnas:
             self.tabla.heading(items, text = items.capitalize())
+            
+    def mostrarRegistros(self, arreglo):
+        for items in arreglo:
+            self.tabla.insert('', tk.END, value = (items[1], items[2], items[3], items[4]))
+            print(items)
         
-    def errorMessage(self, mensaje):
-        messagebox.showerror('Error!', f'{mensaje}')
-        print(f'Hubo un error, validar el logfile | {mensaje}')
+    def errorMessage(self, error):
+        messagebox.showerror('Error!', f'{error}', parent = self.ventanaHabitacion)
+        print(f'Hubo un error, validar el logfile | {error}')
+        
+    def mostrarMensaje(self, mensaje):
+        messagebox.showinfo('Error!', f'{mensaje}', parent = self.ventanaHabitacion)
         
     
     
