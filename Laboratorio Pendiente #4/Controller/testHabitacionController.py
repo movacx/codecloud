bdaa250725e4925dd6ddb6de2916a6af87b87ab8
movacx <fabianbeita36@gmail.com>
2026-02-15 10:38:45 -0800

    update

--- Laboratorio Pendiente #4/Controller/testHabitacionController.py
+++ Laboratorio Pendiente #4/Controller/testHabitacionController.py
@@ -1,8 +1,16 @@
 from View.testHabitacionGUI import HabitacionGUI
+import Data.baseHabitacion as data
 
 class HabitacionController:
     def __init__(self, root):
         self.GUI = HabitacionGUI(root, self)
-        
         self.ventana = root
         
+    
+    def savelog(self, nombreError):
+        try:
+            data.guardarError(nombreError)
+        except Exception as error:
+            print(f'No se pudo establecer la coneccion con log.txt >> data + controller = {error}')
+            
+    def registrarHabitacion
