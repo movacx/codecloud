caedbe111fc7b454119a8e1f72b3ae54e39fc1a9
movacx <fabianbeita36@gmail.com>
2026-02-16 14:09:30 -0600

    asdasd

--- Laboratorio Pendiente #4/mainGUI.py
+++ Laboratorio Pendiente #4/mainGUI.py
@@ -7,16 +7,16 @@ from View.fileReservasGUI import ReservaGUI
 def main():
     try:
         root = tkGUI.Tk()
-        root.geometry('600x300')
+        root.geometry('200x100')
         root.title('Condominios Costa Mar')
 
         contenedor = tkGUI.Frame(root)
         contenedor.grid(row = 0, column = 0)
 
         tkGUI.Button(contenedor, text = 'Huesped', command = lambda: HuespedController(root)).grid(row = 0, column = 0)
-        tkGUI.Button(contenedor, text = 'Reservas', command = lambda: ReservaGUI(root,None)).grid(row = 1, column = 0)
-        tkGUI.Button(contenedor, text = 'Reportes', command = lambda: ReportesController(root)).grid(row = 2, column = 0)
-        tkGUI.Button(contenedor, text = 'Test Habitacion', command = lambda: controllerDavid(root)).grid(row = 3, column = 0)
+        tkGUI.Button(contenedor, text = 'Reservas', command = lambda: ReservaGUI(root,None)).grid(row = 0, column = 1)
+        tkGUI.Button(contenedor, text = 'Reportes', command = lambda: ReportesController(root)).grid(row = 1, column = 0)
+        tkGUI.Button(contenedor, text = 'Test Habitacion', command = lambda: controllerDavid(root)).grid(row = 1, column = 1)
 
         root.mainloop()
 
