f05605d9a4e4b87d4b995fed95c3030a4f4f8a6a
Jos-2006 <camposjoseph136@gmail.com>
2026-02-17 15:20:35 -0600

    a

--- Quiz #3/model/clienteModel.py
+++ Quiz #3/model/clienteModel.py
@@ -1,1 +1,21 @@
-a
+class Cliente:
+    
+    def __init__(self, dni, nombre, apellido, email):
+        self.dni=dni
+        self.nombre=nombre
+        self.apellido=apellido
+        self.email=email
+            
+    def getDni(self):
+        return self.dni
+    
+    def setDni(self,dni):
+        self.dni=dni
+        
+        
+    def importarCsv(self):
+        return [(self.dni, self.nombre, self.apellido, self.email)]
+        
+        
+        
+        
--- Quiz #3/model/cuentaAhorroModel.py
+++ Quiz #3/model/cuentaAhorroModel.py
@@ -1,1 +1,14 @@
-a
+class CuentaAhorro(Cuenta):
+    
+    def __init__(self, numeroCuenta, dniCliente, saldo, interesAnual):
+       super().__init__(numeroCuenta, diniCliente, saldo)
+       self.interesAnual=interesAnual
+       
+       def aplicarInteres(self):
+           interesGanado=self.saldo * self.interesAnual
+           self.saldo+=interesGanado
+           print(f"Interes aplicado: {interesGanado} saldo total: {self.saldo}")
+           
+           
+        def importarCsv(self):
+            return [(self.numeroCuenta, self.dniCliente, self.saldo, self.interesAnual)]
