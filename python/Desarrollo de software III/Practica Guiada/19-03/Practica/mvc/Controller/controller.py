9b58736d30bd7c0a2eddc015267fdedab4f5c79f
movacx <fabianbeita36@gmail.com>
2026-03-19 18:10:30 -0600

    asd
    asd

--- python/Desarrollo de software III/Practica Guiada/19-03/Practica/mvc/Controller/controller.py
+++ python/Desarrollo de software III/Practica Guiada/19-03/Practica/mvc/Controller/controller.py
@@ -1,3 +1,10 @@
+from Model.estudianteModel import Estudiante
+from Model.libroModel import Libro
+from Model.prestamoModel import Prestamo
+from Model.repositorio import Repositorio
+
+
+
 class Controller:
     def __init__(self):
         pass
--- python/Desarrollo de software III/Practica Guiada/19-03/Practica/mvc/main.py
+++ python/Desarrollo de software III/Practica Guiada/19-03/Practica/mvc/main.py
@@ -1,1 +1,7 @@
-print("Hola mundo")
+
+
+def main():
+    print('Hola mundo')
+
+if __name__ == '__main__':
+    main()
