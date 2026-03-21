626116d6108d347c957d92d38a381a1f025d7889
movacx <fabianbeita36@gmail.com>
2026-03-21 04:06:40 +0000

    as

--- Javaa/HolaMundo.java
+++ Javaa/HolaMundo.java
@@ -1,7 +1,11 @@
+import javax.swing.JOptionPane;
+
+
 public class HolaMundo
 {
     public static void main(String[] args)
     {
         System.out.println("Hola mundo");
+        JOptionPane.showMessageDialog(null,"Hola mundo");
     }
 }
