af79a2379b41c5f1cfef0f56bcd8cf1336af5ca4
movacx <fabianbeita36@gmail.com>
2026-03-26 19:49:07 -0600

    asd
    asd

--- python/Desarrollo de software III/Practica Guiada/26-3/Practica guiada/mvc/main.py
+++ python/Desarrollo de software III/Practica Guiada/26-3/Practica guiada/mvc/main.py
@@ -8,29 +8,32 @@ Relacion SOLID
 SRP: Este archivo solamente iniciay controla el ciclo general del programa
 No tiene logica de negocio'''
 
+
+
+
 from View.vista import VistaConsola
 from Controller.controller import Procesador_pago
 
+def main()->None:
 
-
-def main() -> None:
-    '''
-    Funcion principal del sistema
-    ejecuta el men en bucle has que el usuario decida salir
-    return
-    '''
-    controlador = Procesador_pago()
     vista = VistaConsola()
+    controlador = Procesador_pago()
+    while True:
+        try:
+            opcion = vista.mostrar_menu()
 
-    opcion = vista.mostrar_menu()
-    
+            if opcion =="4":
+                vista.mostrar_resultado("Saliendo")
+                break
 
-    if opcion == '0':
-        vista.mostrar_resultado('Saliendo')
-        
+            monto = vista.solicitar_monto()
+            resultado = controlador.procesa_pago(opcion, monto)
+            vista.mostrar_resultado(resultado)
 
-    monto = vista.solicitar_monto()
-    resultado = controlador.procesar_pago(opcion)
+        except ValueError as error:
+            vista.mostrar_error(str(error))
+        except Exception as error:
+            vista.mostrar_error(f"Ocurrio un error en el programa: {error}")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
