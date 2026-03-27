c2db153220366fdf10ec87709965c4eb8203ef06
movacx <fabianbeita36@gmail.com>
2026-03-26 19:44:52 -0600

    asd
    asd

--- python/Desarrollo de software III/Practica Guiada/26-3/Practica guiada/mvc/main.py
+++ python/Desarrollo de software III/Practica Guiada/26-3/Practica guiada/mvc/main.py
@@ -8,11 +8,29 @@ Relacion SOLID
 SRP: Este archivo solamente iniciay controla el ciclo general del programa
 No tiene logica de negocio'''
 
+from View.vista import VistaConsola
+from Controller.controller import Procesador_pago
+
+
+
 def main() -> None:
     '''
     Funcion principal del sistema
     ejecuta el men en bucle has que el usuario decida salir
     return
     '''
-
+    controlador = Procesador_pago()
     vista = VistaConsola()
+
+    opcion = vista.mostrar_menu()
+    
+
+    if opcion == '0':
+        vista.mostrar_resultado('Saliendo')
+        
+
+    monto = vista.solicitar_monto()
+    resultado = controlador.procesar_pago(opcion)
+
+if __name__ == '__main__':
+    main()
