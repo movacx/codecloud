ad05024f2ee336afae1b973f939bce9bce5d5c3d
movacx <fabianbeita36@gmail.com>
2026-04-06 20:29:51 -0600

    asd
    sad

--- python/Desarrollo de software III/Practica Guiada/06-04/Capas/Model/freight_repository.py
+++ python/Desarrollo de software III/Practica Guiada/06-04/Capas/Model/freight_repository.py
@@ -1,6 +1,47 @@
+import os.path
+import json
+from Model.freight import Freight
+
 class FreightRepository:
     '''
     Este repositorio gestiona la persistencia de los fletes
+    SRP: Solo administra persistencia y consultas de fletes
     '''
-    def __init__(self):
-        pass
+
+    #El repositorio recibe el nombre del archivo donde se guardaran los fletes
+
+
+    def __init__(self, filename = 'freights.json'):
+        self.filename = filename
+        
+        #Lista con todos los fletes registrados
+        self._freights=[]
+
+        #Diccionario que agrupa fletes por identificador de cliente
+        #La llave es customer_id y el valor es una lista de fletes
+
+        self._freights_by_customer_id = {}
+
+        #Se intenta cargar la informacion ya guardada
+        #self._load() por el momento no existe
+
+    def _load(self):
+        if not os.path.exists(self.filename):
+            return
+        
+        with open(self.filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+            for item in data:
+                freight = Freight.from_dict(item)
+                self._freights.append(freight)
+
+            #Si no existe una lista para ese cliente, la crea
+
+            if freight.customer_id not in self._freights_by_customer_id:
+                self._freights_by_customer_id[freight.customer_id]=[]
+
+            #luego se agrega el flete a la lista del cliente correspondiente
+            self._freights_by_customer_id[freight.customer_id].append(freight)
+
+    
