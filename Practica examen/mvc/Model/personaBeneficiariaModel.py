25ce7e0b5dd79686585d09642339d5a82e243e1c
movacx <fabianbeita36@gmail.com>
2026-04-23 17:28:23 -0600

    asd
    asd

---
+++ Practica examen/mvc/Model/personaBeneficiariaModel.py
@@ -0,0 +1,13 @@
+class Beneficiaria:
+    def __init__(self, id, nombre, comunidad, cantidadIntegrantes, prioridadSocial):
+        self.id = id
+        self.nombre = nombre
+        self.comunidad = comunidad
+        self.cantidadIntegrantes = cantidadIntegrantes
+        self.prioridadSocial = prioridadSocial
+
+    def __str__(self):
+        return self.id, self.nombre, self.comunidad, self.cantidadIntegrantes, self.prioridadSocial
+
+    def to_dict(self):
+        return 
