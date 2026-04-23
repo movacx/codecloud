6bd93a406cdfcfc9196cb1d0b6e17ee52fae1d37
movacx <fabianbeita36@gmail.com>
2026-04-22 19:47:29 -0600

    asd
    asd

---
+++ Java/Algoritmos/Practica TDA Lista y TDA Objetos/ControlEmpleados.java
@@ -0,0 +1,106 @@
+
+import javax.swing.JOptionPane;
+
+public class ControlEmpleados 
+{
+    String nombreEstudiante;
+    TDAlistaObjeto miLista;
+    int opcionMenu;
+    String cedulaTemp;
+    int edadTemp;
+    int bandera = 0;
+
+    public static void main(String[] args) 
+    {
+        new ControlEmpleados();
+    }
+
+    public ControlEmpleados()
+    {
+        miLista = new TDAlistaObjeto();
+        menuPrincipal();
+    }
+
+    public void menuPrincipal()
+    {
+        do
+        {
+            opcionMenu = Integer.parseInt(JOptionPane.showInputDialog(null,
+                "\n 1. registrar empleado" +
+                "\n 2. buscar por cedula" +
+                "\n 3. buscar empleados con la misma edad" +
+                "\n 4. Ver cantidad total de empleados" +
+                "\n 5. mostrar todo" +
+                "\n 6. borrar todo" +
+                "\n 7. Salir"));
+
+            switch(opcionMenu)
+            {
+                case 1:
+                    registrar();
+                    break;
+                case 2:
+                    buscar();
+                    break;
+                case 3:
+                    contar();
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, "Total: " + miLista.cantidadEmpleados());
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, miLista.mostrarLista());
+                    break;
+                case 6:
+                    miLista.eliminarLista();
+                    JOptionPane.showMessageDialog(null, "Lista limpia");
+                    break;
+                case 7:
+                    JOptionPane.showMessageDialog(null, "Saliendo del sistema...");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Esa opcion no sirve");
+            }
+        } while(opcionMenu != 7);
+    }
+
+    public void registrar()
+    {
+        cedulaTemp = JOptionPane.showInputDialog("Numero de cedula:");
+        edadTemp = Integer.parseInt(JOptionPane.showInputDialog("Edad del empleado:"));
+        
+        Empleado nuevo;
+        nuevo = new Empleado(cedulaTemp, edadTemp);
+        
+        miLista.insertar(nuevo);
+        bandera = 1; 
+        JOptionPane.showMessageDialog(null, "Guardado con exito");
+    }
+
+    public void buscar()
+    {
+        String buscarCed;
+        if(bandera == 0)
+        {
+            JOptionPane.showMessageDialog(null, "Primero registre a alguien");
+        }
+        else
+        {
+            buscarCed = JOptionPane.showInputDialog("Cual cedula busca?");
+            if(miLista.existeEmpleado(buscarCed) == true)
+            {
+                JOptionPane.showMessageDialog(null, "Ese empleado si existe");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "No se encontro");
+            }
+        }
+    }
+    public void contar()
+    {
+        int laEdad;
+        laEdad = Integer.parseInt(JOptionPane.showInputDialog("Edad a buscar:"));
+        JOptionPane.showMessageDialog(null, "Hay " + miLista.contarPorEdad(laEdad) + " empleados");
+    }
+}
---
+++ Java/Practica Gemini Nodos/ManejoNodo.java
@@ -0,0 +1,52 @@
+public class ManejoNodo
+{
+    private Nodo primero; // Apuntador al inicio de la lista
+    
+    public ManejoNodo()
+    {
+        this.primero = null; // Al inicio, la lista esta vacia=
+    }
+    
+    public boolean esVacio()
+    {
+        return primero == null;
+    }
+    
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=//
+    
+    private Nodo obtenerNodo(int posicion)
+    {
+        Nodo auxiliar = primero;
+        int contador = 1;
+        
+        while (aux != null && contador < posicion)
+        {
+            aux = aux.getSiguiente();
+            contador++;
+        }
+    }
+    
+    public void insertar(int dato, int posicion)
+    {
+        Nodo nuevoNodo = new Nodo(dato);
+        //Caso A el vagon va de primero
+        //Si la lista esta vacia o el usuario pide la posicion 1
+        
+        if (esVacio() || posicion == 1)
+        {
+            nuevoNodo.setSiguiente(primero); //El nuevo ahora paunta al que era el primero
+            primero =  nuevoNodo;            //El inicio de la lista ahora es el nuevo
+        }else
+        { //Caso B: El vagon va en medio o al final
+            //Le pedimos al asistente el nodo anterior a donde queremos insertar
+            Nodo anterior = obtenerNodo(posicion - 1);
+            
+            if (anterior != null)
+            {
+                nuevoNodo.setSiguiente(anterior.getSiguiente());
+                anterior.setSiguiente(nuevoNodo)
+            }
+        }
+    }
+    
+}
