a52f830d5b2a560acca0390bcba8b94dbf4102f0
movacx <fabianbeita36@gmail.com>
2026-05-05 14:33:40 -0600

    commit
    Se acomodaron los archivos.

--- python/Desarrollo de software II/Actividades/Python/Curso de python/inputs/input_nombres.py
+++
@@ -1,5 +0,0 @@
-#pedirle un dato al usuario
-
-nombre = input("che maestro, dame tu nombre: ")
-
-print(f"El nombre del usuario es: {nombre}")
--- python/Desarrollo de software II/Actividades/Python/Curso de python/variables 2.0/tuplas.py
+++
@@ -1,9 +0,0 @@
-#creando tuplas con tuble()
-tupla = tuple(["dato1","dato2"])
-
-#creando una tupla sin parentesis de multiples datos
-tupla = "datoUno","datoDos"
-
-#creando una tupla sin parentesis de un solo dato
-tupla = "dato"
-print(tupla)
