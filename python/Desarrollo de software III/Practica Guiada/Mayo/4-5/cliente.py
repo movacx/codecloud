e8db7dabab8580bd818d363cd9c4e4fe8d70ebbb
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-05-04 19:32:43 -0600

    asd

--- python/Desarrollo de software III/Practica Guiada/Mayo/4-5/cliente.py
+++ python/Desarrollo de software III/Practica Guiada/Mayo/4-5/cliente.py
@@ -1,21 +1,23 @@
 import socket
 
-#configuraciones
-direccion_ip= "10.35.118.213"
-puerto=80
+#Configuraciones
+direccion_ip = "10.35.118.211"
+puerto = 80
 
-socket_cliente= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-socket_cliente.connect((direccion_ip, puerto))
-socket_cliente.sendall(bytes("Hola, soy un ciente!!", "utf-8"))
+socket_Cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_Cliente.connect((direccion_ip, puerto))
+socket_Cliente.sendall(bytes("David Mora saluda a Herlin", 'utf-8'))
 
 while True:
-    in_data=socket_cliente.recv(1024)
+    in_data = socket_Cliente.recv(1024)
 
-    print("Desde  el servidor: ", in_data.decode("UTF-8"))
-    out_data=input()
-    socket_cliente.sendall(bytes(out_data, "UTF-8"))
+    print("Desde el servidor: ", in_data.decode())
+    out_data = input()
+    socket_Cliente.sendall(bytes(out_data, 'utf-8'))
 
-    if out_data== 'end':
+    if out_data == 'end':
         break
 
-socket_cliente.close()
+socket_Cliente.close()
+
+
