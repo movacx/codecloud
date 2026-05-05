0d3eb525a47a33a29bdef750443e57899db3bca5
GabrielMR-code <gbjosuemr1@gmail.com>
2026-05-04 19:16:31 -0600

    Update cliente.py
    asd

--- python/Desarrollo de software III/Practica Guiada/Mayo/4-5/cliente.py
+++ python/Desarrollo de software III/Practica Guiada/Mayo/4-5/cliente.py
@@ -1,10 +1,10 @@
 import socket
 #configuraciones
-direccion_ip = '10.35.118.152'
+direccion_ip = "10.35.118.152"
 puerto = 80
 
 socket_cliente = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-socket_cliente.connect(direccion_ip,puerto)
+socket_cliente.connect((direccion_ip,puerto))
 socket_cliente.sendall(bytes('Hola, sou un nuevo cliente!!', 'UTF-8'))
 
 while True:
